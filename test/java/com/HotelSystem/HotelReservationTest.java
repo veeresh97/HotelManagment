25b9324f59cfeba77d956c71f5ab8e720bff0864
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-21 10:52:38 +0530

    Modified: code and indentation

--- test/java/com/HotelSystem/HotelReservationTest.java
+++ test/java/com/HotelSystem/HotelReservationTest.java
@@ -2,19 +2,11 @@ package com.HotelSystem;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.util.List;
 
 public class HotelReservationTest {
+
     @Test
-    public void givenHotel_whenInvokeAddHotel_shouldBeAbleToAdd() {
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotel hotel = new Hotel("Lakewood",110);
-        hotelReservationService.addHotel(hotel);
-        List hotelList = hotelReservationService.getHotels();
-        Assertions.assertTrue(hotelList.contains(hotel));
-    }
-    @Test
-    public void given3Hotels_whenInvokeFindCheapestHotel_shouldBeAbleToReturnCheapestHotel() {
+    public void given3Hotels_whenInvokedFindCheapestHotel_shouldBeAbleToReturnCheapestHotel() {
         HotelReservationService hotelReservationService = new HotelReservationService();
         Hotel Lakewood = new Hotel("Lakewood",110);
         Hotel Bridgewood = new Hotel("Bridgewood",160);
